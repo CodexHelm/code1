60f95e015b3355e0d9fcc4e30910b63222b04894
CodexHelm <codexhelm01@gmail.com>
2024-03-22 22:00:00 -0400

    .

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log("Here")
-    res.render('index')
+    res.render("index", { text: 'World'})
 })
 app.listen(3000)
